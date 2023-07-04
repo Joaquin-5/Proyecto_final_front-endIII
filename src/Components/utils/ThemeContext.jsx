bf1c732c2fd97d8d9c17ac098e57b4352389ca2f
= <=>
2023-07-04 00:05:19 -0300

    Changes on provider file

--- src/Components/utils/ThemeContext.jsx
+++ src/Components/utils/ThemeContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const ThemeContext = createContext(null);
 
-export const ThemeContextProvider = (props) => {
+export const ThemeProvider = (props) => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
   const toggleTheme = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./Components/Navbar.jsx";
 import Footer from "./Components/Footer.jsx";
-import { ThemeContextProvider } from "./Components/utils/themeContext.jsx";
+import { ThemeProvider } from "./Components/utils/themeContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContextProvider>
+    <ThemeProvider>
       <BrowserRouter>
         <Navbar />
         <App />
         <Footer />
       </BrowserRouter>
-    </ThemeContextProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
